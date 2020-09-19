b52672d559db019554bea67a4f53e61f8dc87c9e
tomoshimizu <53202433+tomoshimizu@users.noreply.github.com>
2020-09-19 15:55:40 +0900

    Initial Commit

---
+++ voicediary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  voicediary
+//
+//  Created by 志水智 on 2020/09/19.
+//  Copyright © 2020 志水智. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
